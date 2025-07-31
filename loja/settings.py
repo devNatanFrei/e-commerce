13e28cdedf598fe3a2ce394db3e6ffcb422412c0
Natan Freitas <natanael_freitas@outlook.com>
2025-07-31 10:37:39 -0300

    Update settings.py

--- loja/settings.py
+++ loja/settings.py
@@ -101,10 +101,15 @@ TEMPLATES = [
 #
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
+
 
 #
 # Validação de senhas
